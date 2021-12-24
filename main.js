9412aeaa475fec1bb5fb79ee681becb8f15ab515
Abhinov <abhinov.in@gmail.com>
2021-12-24 21:05:45 +0530

    fixed glow and prompt is now bolder

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ var commands = {
         this.echo('MVDZO X AVIN');
     },
     help: function () {
+        playAudio();
         this.echo(showHelp());
     },
     hie: function (name) {
@@ -81,6 +82,7 @@ var commands = {
         this.echo(phoneAPI(phoneNumber));
     },
     alexa: function () {
+        playAudio()
         this.echo(foaas());
     },
     gender: function () {
@@ -135,6 +137,21 @@ var commands = {
     }
 };
 
+function playAudio() {
+    console.log("playing audio")
+    const audio_list = [
+                        "./salamisound-1772817-elma-4-short.mp3",
+                        "./salamisound-2806608-elma-9-electromechanical.mp3",
+                        "./salamisound-3322341-elma-7-electromechanical.mp3",
+                        "./salamisound-5643712-elma-5-electro-mechanical.mp3",
+                        "./salamisound-6796298-elma-3-electro-mechanical.mp3"
+                    ]
+    var random_audio = audio_list[Math.floor(Math.random()*audio_list.length)];
+    var audio = new Audio(random_audio);
+    console.log("playing audio :: "+audio)
+    audio.play();
+}
+
 
 function completion(string, callback) {
     var command = this.get_command();
@@ -181,94 +198,22 @@ function completion(string, callback) {
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-$('body').terminal(
-{
-    iam: function (name) {
-        this.echo('Hello, ' + name +
-            '. Welcome to TERMINAL UI.');
-    },
-    developer: function () {
-        this.echo('MVDZO X AVIN');
-    },
-    help: function () {
-        this.echo(showHelp());
-    },
-    hie: function (name) {
-        this.echo(saysomething(name));
-    },
-    advice: function () {
-        this.echo(fetchAdviceAPI());
-    },    
-    weather: function (city_name) {
-        this.echo(weatherAPI(city_name));
-    },
-    phone: function (phoneNumber) {
-        this.echo(phoneAPI(phoneNumber));
-    },
-    alexa: function () {
-        this.echo(foaas());
-    },
-    whatsmyip : function () {
-        this.echo(whatsmyIP());
-    },
-    gender : function (name) {
-        this.echo(gender(name));
-    },
-    cd: function(dir) {
-        this.pause();
-        if (dir === '/') {
-            path = [];
-            cwd = restore_cwd(fs, path);
-        } else if (dir === '..') {
-            if (path.length) {
-                path.pop(); // remove from end
-                cwd = restore_cwd(fs, path);
-            }
-        } else if (dir.match(/\//)) {
-            var p = dir.replace(/\/$/, '').split('/').filter(Boolean);
-            if (dir[0] !== '/') {
-                p = path.concat(p);
-            }
-            cwd = restore_cwd(fs, p);
-            path = p;
-        } else if (!is_dir(cwd[dir])) {
-            this.error($.terminal.escape_brackets(dir) + ' is not a directory');
-        } else {
-            cwd = cwd[dir];
-            path.push(dir);
-        }
-        this.resume();
-    },
-    ls: function() {
-        if (!is_dir(cwd)) {
-            throw new Error('Internal Error Invalid directory');
-        }
-        var dir = Object.keys(cwd).map(function(key) {
-            if (is_dir(cwd[key])) {
-                return key + '/';
-            }
-            return key;
-        });
-        this.echo(dir.join('\n'));
-    },
-    cat: function(file) {
-        if (!is_file(cwd[file])) {
-            this.error($.terminal.escape_brackets(file) + " don't exists");
-        } else {
-            this.echo(cwd[file]);
-        }
-    }
-},
-{
+$('body').terminal(commands,
+    {
     completion: completion,
     name: 'Terminal Jarvis v0.1',
     prompt: prompt(),
     greetings: 'Terminal Jarvis v0.1 - :: Type \'help\' to show options\n'+ASCII_TEXT+showHelp(),
-});
+    exceptionHandler: exceptionHandlerFn()
+    }
+);
 
 function showHelp() {
     return HELP;
+}
+
+function exceptionHandlerFn() {
+    console.log("Something went wrong")
 }
 
 function saysomething(name) {
