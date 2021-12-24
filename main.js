df930ca8c361a4200ad3eb31fdb23baf658b5109
Abhinov <abhinov.in@gmail.com>
2021-12-24 13:35:39 +0530

    file directory, retro look

--- main.js
+++ main.js
@@ -4,10 +4,55 @@ const ASCII_TEXT =
 "   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║███████║██║         ██║   ██║██║\n"+
 "   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██╔══██║██║         ██║   ██║██║\n"+
 "   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║███████╗    ╚██████╔╝██║\n"+
-"   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═╝\n";                                                                           
+"   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝     ╚═════╝ ╚═╝\n"; 
 
-$('body').terminal(
-{
+const HELP = 
+    '\nhelp                              :: to show this menu\n'
+    +"iam <your name>                   :: for greeting\n"
+    +"developer                         :: to know the Anonymous\n"
+    +"advice                            :: to get a free advice\n"
+    +"weather <city>                    :: check your city's weather\n"
+    +"phone <country code><phone number>:: to get details of your number\n"
+    +"alexa                             :: chat with alexa\n"
+    +"whatsmyip                         :: to get details of your ip\n"
+
+// Fake in memory filesystem
+var fs = {
+    'projects': {
+        'baz.txt': 'Hello this is file baz.txt',
+        'quake.txt': "Cras sit amet semper arcu. Donec est nunc, dapibus non scelerisque quis, pellentesque at justo. Nunc nec vehicula ante, at dapibus orci. In ultricies rutrum ante, dictum placerat lorem faucibus vitae. Aliquam porttitor at mauris id semper. Integer rutrum sem id nunc congue congue. Etiam dignissim diam in dui ultrices, a venenatis velit ultricies. Maecenas ac ultrices mi. Nunc molestie sapien vitae massa vulputate iaculis. Phasellus vehicula laoreet mi, vel bibendum nisl laoreet sit amet. Fusce sit amet rutrum lacus.",
+        'foo.txt': "Hello, World!",
+        'bar.txt': "Wellcome to the bar",
+        "terminal": {
+            "foo": {
+                "bar.txt": "hello bar",
+                "baz.txt": "baz content"
+            }
+        }
+    }
+};
+
+var path = [];
+var cwd = fs;
+function restore_cwd(fs, path) {
+    path = path.slice();
+    while (path.length) {
+        var dir_name = path.shift();
+        if (!is_dir(fs[dir_name])) {
+            throw new Error('Internal Error Invalid directory ' +
+                            $.terminal.escape_brackets(dir_name));
+        }
+        fs = fs[dir_name];
+    }
+    return fs;
+}
+function is_dir(obj) {
+    return typeof obj === 'object';
+}
+function is_file(obj) {
+    return typeof obj === 'string';
+}
+var commands = {
     iam: function (name) {
         this.echo('Hello, ' + name +
             '. Welcome to TERMINAL UI.');
@@ -16,13 +61,7 @@ $('body').terminal(
         this.echo('MVDZO X AVIN');
     },
     help: function () {
-        this.echo(
-          'help                             :: to show this menu\n'
-        +'iam <your name>                  :: for greeting\n'
-        +'developer                        :: to know the Anonymous\n'
-        +'advice                            :: to get a free advice\n'
-        +"weather <city name>               :: check your city's weather\n"
-        +"phone <country code><phone number>:: to get details of your number\n");
+        this.echo(showHelp());
     },
     hie: function (name) {
         this.echo(saysomething(name));
@@ -38,14 +77,191 @@ $('body').terminal(
     },
     alexa: function () {
         this.echo(foaas());
+    },
+    whatsmyip : function () {
+        this.echo(whatsmyIP());
+    },
+    cd: function(dir) {
+        this.pause();
+        if (dir === '/') {
+            path = [];
+            cwd = restore_cwd(fs, path);
+        } else if (dir === '..') {
+            if (path.length) {
+                path.pop(); // remove from end
+                cwd = restore_cwd(fs, path);
+            }
+        } else if (dir.match(/\//)) {
+            var p = dir.replace(/\/$/, '').split('/').filter(Boolean);
+            if (dir[0] !== '/') {
+                p = path.concat(p);
+            }
+            cwd = restore_cwd(fs, p);
+            path = p;
+        } else if (!is_dir(cwd[dir])) {
+            this.error($.terminal.escape_brackets(dir) + ' is not a directory');
+        } else {
+            cwd = cwd[dir];
+            path.push(dir);
+        }
+        this.resume();
+    },
+    ls: function() {
+        if (!is_dir(cwd)) {
+            throw new Error('Internal Error Invalid directory');
+        }
+        var dir = Object.keys(cwd).map(function(key) {
+            if (is_dir(cwd[key])) {
+                return key + '/';
+            }
+            return key;
+        });
+        this.echo(dir.join('\n'));
+    },
+    cat: function(file) {
+        if (!is_file(cwd[file])) {
+            this.error($.terminal.escape_brackets(file) + " don't exists");
+        } else {
+            this.echo(cwd[file]);
+        }
+    } 
+};
+function completion(string, callback) {
+    var command = this.get_command();
+    var cmd = $.terminal.parse_command(command);
+    function dirs(cwd) {
+        return Object.keys(cwd).filter(function(key) {
+            return is_dir(cwd[key]);
+        }).map(function(dir) {
+            return dir + '/';
+        });
+    }
+    if (cmd.name === 'ls') {
+        callback([]);
+    } else if (cmd.name === 'cd') {
+        var p = string.split('/').filter(Boolean);
+        if (p.length === 1) {
+            if (string[0] === '/') {
+                callback(dirs(fs));
+            } else {
+                callback(dirs(cwd));
+            }
+        } else {
+            if (string[0] !== '/') {
+                p = path.concat(p);
+            }
+            if (string[string.length - 1] !== '/') {
+                p.pop();
+            }
+            var prefix = string.replace(/\/[^/]*$/, '');
+            callback(dirs(restore_cwd(fs, p)).map(function(dir) {
+                return prefix + '/' + dir;
+            }));
+        }
+    } else if (cmd.name === 'cat') {
+        var files = Object.keys(cwd).filter(function(key) {
+            return is_file(cwd[key]);
+        });
+        callback(files);
+    } else {
+        callback(Object.keys(commands));
+    }
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+$('body').terminal(
+{
+    iam: function (name) {
+        this.echo('Hello, ' + name +
+            '. Welcome to TERMINAL UI.');
+    },
+    developer: function () {
+        this.echo('MVDZO X AVIN');
+    },
+    help: function () {
+        this.echo(showHelp());
+    },
+    hie: function (name) {
+        this.echo(saysomething(name));
+    },
+    advice: function () {
+        this.echo(fetchAdviceAPI());
+    },    
+    weather: function (city_name) {
+        this.echo(weatherAPI(city_name));
+    },
+    phone: function (phoneNumber) {
+        this.echo(phoneAPI(phoneNumber));
+    },
+    alexa: function () {
+        this.echo(foaas());
+    },
+    whatsmyip : function () {
+        this.echo(whatsmyIP());
+    },
+    cd: function(dir) {
+        this.pause();
+        if (dir === '/') {
+            path = [];
+            cwd = restore_cwd(fs, path);
+        } else if (dir === '..') {
+            if (path.length) {
+                path.pop(); // remove from end
+                cwd = restore_cwd(fs, path);
+            }
+        } else if (dir.match(/\//)) {
+            var p = dir.replace(/\/$/, '').split('/').filter(Boolean);
+            if (dir[0] !== '/') {
+                p = path.concat(p);
+            }
+            cwd = restore_cwd(fs, p);
+            path = p;
+        } else if (!is_dir(cwd[dir])) {
+            this.error($.terminal.escape_brackets(dir) + ' is not a directory');
+        } else {
+            cwd = cwd[dir];
+            path.push(dir);
+        }
+        this.resume();
+    },
+    ls: function() {
+        if (!is_dir(cwd)) {
+            throw new Error('Internal Error Invalid directory');
+        }
+        var dir = Object.keys(cwd).map(function(key) {
+            if (is_dir(cwd[key])) {
+                return key + '/';
+            }
+            return key;
+        });
+        this.echo(dir.join('\n'));
+    },
+    cat: function(file) {
+        if (!is_file(cwd[file])) {
+            this.error($.terminal.escape_brackets(file) + " don't exists");
+        } else {
+            this.echo(cwd[file]);
+        }
     }
 },
 {
-    greetings: 'Terminal UI - Hack TTS NASA :: Type \'help\' to show options\n'+ASCII_TEXT+speakText("Welcome to Terminal U.I")
+    completion: completion,
+    name: 'Terminal Jarvis v0.1',
+    prompt: 'user@jarvis-v1.0: ',
+    greetings: 'Terminal Jarvis v0.1 - :: Type \'help\' to show options\n'+ASCII_TEXT+showHelp(),
 });
 
+function showHelp() {
+    return HELP;
+}
+
 function saysomething(name) {
-    speakText('Hieee ' +name+ '.Welocoome from say something.')
+    speakText('Hieee ' +name+ '.Welcome from say something.')
     return 'Hieee ' +name+ '..Weocooooooooooooomeeeeeeeeeeeeeeeee from saysomething.';
 }
 
@@ -61,14 +277,16 @@ async function weatherAPI(city) {
     let api_key = "a96ddd921cbd4cc62683b3fa95c67cd2"
     let url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key;
     let response = await fetch(url);
-    console.log(response)
     let data = await response.text();
-    console.log(data);
     parsed_response = JSON.parse(data);
     console.log(parsed_response);
-    console.log(parsed_response.weather[0])
-    console.log(parsed_response.weather[0].main)
-    return "Weather in "+city+" is : "+parsed_response.weather[0].main+" :: "+parsed_response.weather[0].description
+    if (parsed_response.cod.toString() === '404') {
+        return parsed_response.message;
+    }
+    let temp = (parsed_response.main.temp)-273.15;
+    return "Weather in "+city+", "+parsed_response.sys.country+
+    " is : "+parsed_response.weather[0].main+" : "+parsed_response.weather[0].description
+    +" \nTemperture is "+temp.toString().substring(0, 4)+"°C"
 
 }
 
@@ -77,7 +295,7 @@ async function phoneAPI(phone_no) {
     console.log(phone_no.toString());
     console.log(phone_no.length);
     let api_key = "3be95701c94b46ec802a1f16b74818a8"
-    let url = "https://phonevalidation.abstractapi.com/v1/?api_key=3be95701c94b46ec802a1f16b74818a8&phone="+phone_no;
+    let url = "https://phonevalidation.abstractapi.com/v1/?api_key="+api_key+"&phone="+phone_no;
     let response = await fetch(url);
     console.log(response)
     let data = await response.text();
@@ -102,7 +320,7 @@ async function phoneAPI(phone_no) {
 async function foaas() {
     const url_array = ['zero', 'asshole', 'awesome', 'bag', 'bucket', 'bye', 'cool', 'cup', 'dense', 'diabetes', 'dumbledore', 'even', 'everyone', 'everything', 'everything', 'family', 'fascinating', 'flying', 'ftfy', 'fyyff', 'give', 'holygrail', 'horse', 'idea', 'immensity', 'jinglebells', 'life', 'logs', 'looking', 'lowpoly', 'maybe', 'me', 'mornin', 'no', 'pink', 'question', 'ratsarse', 'retard', 'ridiculous', 'rtfm', 'sake', 'shit', 'single', 'thanks', 'that', 'this', 'too', 'tucker', 'what', 'yeah', 'zayn', ''];
     var random_url = url_array[Math.floor(Math.random()*url_array.length)];
-    url = "https://foaas.com/"+random_url+"/sharma";
+    url = "https://foaas.com/"+random_url+"/terminalUI";
     let response = await fetch(url, { headers: new Headers({'Accept': 'application/json'})});
     let data = await response.text();
     console.log(data)
@@ -110,6 +328,27 @@ async function foaas() {
     console.log(response)
     speakText(parsed_response.message)
     return parsed_response.message
+}
+
+async function whatsmyIP() {
+    let url = "https://jsonip.com";
+    let response = await fetch('https://jsonip.com', { mode: 'cors' });
+    let data = await response.text();
+    parsed_response = JSON.parse(data);
+    let ip =  parsed_response.ip;
+    const url_ip_details = "http://ip-api.com/json/"+ip.toString();
+    let response2 = await fetch(url_ip_details, { mode: 'cors' });
+    let data2 = await response2.text();
+    parsed_response2 = JSON.parse(data2);
+    console.log(parsed_response2);
+    if (parsed_response2.status === 'success') {
+        let country = parsed_response2.country;
+        let city = parsed_response2.city;
+        let isp =  parsed_response2.isp;
+        let regionName =  parsed_response2.regionName;
+        return parsed_response.ip +"\n\n"+country+"\n"+city+"\n"+isp+"\n"+regionName;
+    }
+    return parsed_response.ip;
 }
 
 function getSelectionText() {
